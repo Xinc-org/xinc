8c8130694db9c1472b311abeed55f832a2a2bf75
arnoschn <arnoschn@gmail.com>
2008-03-01 19:27:16 +0000

    Issue 115: Enduser Documentation

---
+++ docs/xinc_guide/book/js/Chapters.js
@@ -0,0 +1,9 @@
+var Chapters = new Array(new Array('chapters/AboutThisBook.html','About this book'),
+new Array('chapters/Introduction.html','Introduction'),
+new Array('chapters/Setup.html','Setting-Up Xinc'),
+new Array('chapters/GettingStarted.html','Getting Started'),
+new Array('chapters/appendixes/AppendixA-FactSheet.html','Appendix A: Fact Sheet'),
+new Array('chapters/appendixes/AppendixB-CoreTasks.html','Appendix B: Core Tasks'),
+new Array('chapters/appendixes/AppendixC-ModificationSet.html','Appendix C: ModificationSet'),
+new Array('chapters/appendixes/AppendixD-Builders.html','Appendix D: Builders'),
+new Array('chapters/appendixes/AppendixE-Publishers.html','Appendix D: Publishers'));
