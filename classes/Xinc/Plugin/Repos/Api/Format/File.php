21f4249378ea6dd36389f62ea98ed6b43326cc68
arnoschn <arnoschn@gmail.com>
2008-03-02 13:03:00 +0000

    fixing css content-type

--- classes/Xinc/Plugin/Repos/Api/Format/File.php
+++ classes/Xinc/Plugin/Repos/Api/Format/File.php
@@ -70,6 +70,8 @@ class Xinc_Plugin_Repos_Api_Format_File implements Xinc_Api_Response_Format_Inte
             preg_match('/^.*?.tar$/', $fileName) ||  
             preg_match('/^.*?.tgz$/', $fileName)) {
             return 'application/x-gzip';
+        } else if (preg_match('/.*?\.css/', $fileName)) {
+            return 'text/css';
         }
         /**if (function_exists('finfo_open')) {
             $finfo = finfo_open(FILEINFO_MIME); // return mime type ala mimetype extension
