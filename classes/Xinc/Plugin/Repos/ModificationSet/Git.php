6eb69ba173588f00bf7b25b70f2df0e2dd037a3e
Alexander Opitz <opi99@users.noreply.github.com>
2011-11-08 06:24:43 +0000

    Issue 215: Add git support

---
+++ classes/Xinc/Plugin/Repos/ModificationSet/Git.php
@@ -0,0 +1,148 @@
+<?php
+declare(encoding = 'utf-8');
+/**
+ * Xinc - Continuous Integration.
+ *
+ * PHP version 5
+ *
+ * @category  Development
+ * @package   Xinc.Plugin.Repos.ModificationSet
+ * @author    Alexander Opitz <opitz.alexander@gmail.com>
+ * @copyright 2011 Alexander Opitz, Leipzig
+ * @license   http://www.gnu.org/copyleft/lgpl.html GNU/LGPL, see license.php
+ *            This file is part of Xinc.
+ *            Xinc is free software; you can redistribute it and/or modify
+ *            it under the terms of the GNU Lesser General Public License as
+ *            published by the Free Software Foundation; either version 2.1 of
+ *            the License, or (at your option) any later version.
+ *
+ *            Xinc is distributed in the hope that it will be useful,
+ *            but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *            GNU Lesser General Public License for more details.
+ *
+ *            You should have received a copy of the GNU Lesser General Public
+ *            License along with Xinc, write to the Free Software Foundation,
+ *            Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @link      http://code.google.com/p/xinc/
+ */
+
+require_once 'Xinc/Plugin/Base.php';
+
+require_once 'Xinc/Plugin/Repos/ModificationSet/Git/Task.php';
+require_once 'Xinc/Ini.php';
+/*require_once 'Xinc/Logger.php';
+require_once 'Xinc/Exception/ModificationSet.php';
+require_once 'Xinc/Plugin/Repos/ModificationSet/AbstractTask.php';
+require_once 'Xinc/Plugin/Repos/ModificationSet/Result.php';
+*/
+class Xinc_Plugin_Repos_ModificationSet_Git extends Xinc_Plugin_Base
+{
+    private $strPath;
+
+    public function __construct()
+    {
+        try {
+            $this->strPath = Xinc_Ini::getInstance()->get('path', 'git');
+        } catch (Exception $e) {
+            $this->strPath = 'git';
+        }
+    }
+
+    public function getTaskDefinitions()
+    {
+        return array(new Xinc_Plugin_Repos_ModificationSet_Git_Task($this));
+    }
+
+    protected function _getChangeLog(
+        Xinc_Build_Interface $build, $dir,
+        Xinc_Plugin_Repos_ModificationSet_Result &$set,
+        $fromRevision, $toRevision, $username, $password
+    ) {
+    }
+
+    protected function _getModifiedFiles(
+        Xinc_Build_Interface $build, $dir,
+        Xinc_Plugin_Repos_ModificationSet_Result &$set,
+        $username, $password
+    ) {
+    }
+
+    private function _update(
+        Xinc_Build_Interface $build, $dir,
+        Xinc_Plugin_Repos_ModificationSet_Result &$set,
+        $username, $password
+    ) {
+    }
+
+    /**
+     * Masks certain string elements with **** and returns the string
+     *
+     * @param string $inputStr
+     * @param array $maskElements
+     *
+     * @return string
+     */
+    private function _maskOutput($inputStr, array $maskElements)
+    {
+        $outputStr = str_replace($maskElements, '****', $inputStr);
+        return $outputStr;
+    }
+
+    /**
+     * Checks whether the Subversion project has been modified.
+     *
+     * @return boolean
+     */
+    public function checkModified(Xinc_Build_Interface &$build,
+                                 $dir, $update = false,
+                                 $username = null, $password = null)
+    {
+    }
+
+    /**
+     * Parse the result of an svn command for the Subversion project URL.
+     *
+     * @param string $result
+     *
+     * @return string
+     * @throws Exception
+     */
+    private function getUrl($result)
+    {
+        $xml = new SimpleXMLElement($result);
+        $urls = $xml->xpath('/info/entry/url');
+        $url = (string) $urls[0];
+        return $url;
+    }
+
+    /**
+     * Parse the result of an svn command 
+     * for the Subversion project revision number.
+     *
+     * @param string $result
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function getRevision($result)
+    {
+    }
+
+
+    /**
+     * Check necessary variables are set
+     *
+     * @throws Xinc_Exception_MalformedConfig
+     */
+    public function validate()
+    {
+        if (!@include_once 'VersionControl/Git.php') {
+            throw new Xinc_Exception_MalformedConfig(
+                'PEAR:VersionControl_Git not installed.'
+            );
+        }
+
+        return true;
+    }
+}
---
+++ classes/Xinc/Plugin/Repos/ModificationSet/Git/Task.php
@@ -0,0 +1,152 @@
+<?php
+declare(encoding = 'utf-8');
+/**
+ * Xinc - Continuous Integration.
+ *
+ * PHP version 5
+ *
+ * @category  Development
+ * @package   Xinc.Plugin.Repos.ModificationSet.Git
+ * @author    Alexander Opitz <opitz.alexander@gmail.com>
+ * @copyright 2011 Alexander Opitz, Leipzig
+ * @license   http://www.gnu.org/copyleft/lgpl.html GNU/LGPL, see license.php
+ *            This file is part of Xinc.
+ *            Xinc is free software; you can redistribute it and/or modify
+ *            it under the terms of the GNU Lesser General Public License as
+ *            published by the Free Software Foundation; either version 2.1 of
+ *            the License, or (at your option) any later version.
+ *
+ *            Xinc is distributed in the hope that it will be useful,
+ *            but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *            GNU Lesser General Public License for more details.
+ *
+ *            You should have received a copy of the GNU Lesser General Public
+ *            License along with Xinc, write to the Free Software Foundation,
+ *            Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @link      http://xincplus.sourceforge.net
+ */
+
+require_once 'Xinc/Plugin/Repos/ModificationSet/AbstractTask.php';
+
+class Xinc_Plugin_Repos_ModificationSet_Git_Task
+    extends Xinc_Plugin_Repos_ModificationSet_AbstractTask
+{
+    /**
+     * Directory containing the Git project.
+     *
+     * @var string
+     */
+    private $_directory = '.';
+
+    private $_update = false;
+
+    private $_username = null;
+
+    private $_password = null;
+
+    private $_property;
+
+    public function getName()
+    {
+        return 'git';
+    }
+
+    public function registerTask(Xinc_Plugin_Task_Interface $task)
+    {
+        $this->_subtasks[]=$task;
+    }
+
+    /**
+     * Sets the svn checkout directory.
+     *
+     * @param string
+     */
+    public function setDirectory($directory)
+    {
+        $this->_directory = (string)$directory;
+    }
+
+    /**
+     * sets the name of the property, which will be set to
+     * TRUE in case a modification was detected
+     *
+     * @param string $property
+     */
+    public function setProperty($property)
+    {
+        $this->_property = (string) $property;
+    }
+
+    /**
+     * Sets the username for the svn commands
+     *
+     * @param string
+     */
+    public function setUsername($username)
+    {
+        $this->_username = (string)$username;
+    }
+
+    /**
+     * Sets the password for the svn commands
+     *
+     * @param string
+     */
+    public function setPassword($password)
+    {
+        $this->_password = (string)$password;
+    }
+
+    /**
+     * Tells whether to update the working copy directly here or not
+     *
+     * @param string $update
+     */
+    public function setUpdate($update)
+    {
+        $update = (string) $update;
+        $this->_update = in_array($update, array('true', '1')) ? true:false;
+    }
+
+    public function getPluginSlot()
+    {
+        return Xinc_Plugin_Slot::PRE_PROCESS;
+    }
+
+    public function checkModified(Xinc_Build_Interface $build)
+    {
+        $res = $this->_plugin->checkModified($build, $this->_directory,
+                                             $this->_update, $this->_username,
+                                             $this->_password);
+/*        if ($res->isChanged() && !empty($this->_property)) {
+            // a modification in this tag has been detected
+            $build->getProperties()->set($this->_property, true);
+            $build->info("Property '".$this->_property."' set to TRUE");
+        }*/
+        return $res;
+    }
+
+    public function validateTask()
+    {
+        if (!isset($this->_directory)) {
+            throw new Xinc_Exception_MalformedConfig('Element modificationSet/svn'
+                                                    . ' - required attribute '
+                                                    . '\'directory\' is not set');
+            // @codeCoverageIgnoreStart
+        }
+            // @codeCoverageIgnoreEnd
+        $file = $this->_directory;
+        $file2 = Xinc::getInstance()->getWorkingDir() . DIRECTORY_SEPARATOR . $file;
+        if (!file_exists($file) && !file_exists($file2)) {
+            Xinc_Logger::getInstance()->error('Directory '.$file2.' does not exist');
+            return false;
+        } else if (file_exists($file2)) {
+            $this->_directory = $file2;
+            //Xinc_Logger::getInstance()->error("Directory $file2 does not exist");
+            //return false;
+        }
+        //return false;
+        return true;
+    }
+}
