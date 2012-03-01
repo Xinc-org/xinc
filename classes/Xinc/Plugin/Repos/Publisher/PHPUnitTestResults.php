ac09b3e62b299afe8b0c890dfb074b4bf49f5bbb
Alexander Opitz <opi99@users.noreply.github.com>
2012-03-01 11:28:55 +0000

    Issue 171: Fix problem with empty xml files and symbolic link in path.

--- classes/Xinc/Plugin/Repos/Publisher/PHPUnitTestResults.php
+++ classes/Xinc/Plugin/Repos/Publisher/PHPUnitTestResults.php
@@ -8,6 +8,7 @@ declare(encoding = 'utf-8');
  * @category  Development
  * @package   Xinc.Plugin.Publisher
  * @author    Arno Schneider <username@example.org>
+ * @author    Alexander Opitz <opitz.alexander@gmail.com>
  * @copyright 2007 Arno Schneider, Barcelona
  * @license   http://www.gnu.org/copyleft/lgpl.html GNU/LGPL, see license.php
  *            This file is part of Xinc.
@@ -37,9 +38,9 @@ class Xinc_Plugin_Repos_Publisher_PHPUnitTestResults extends Xinc_Plugin_Base
    
     public function validate()
     {
-       
         return true;
     }
+
     public function getTaskDefinitions()
     {
         return array(new Xinc_Plugin_Repos_Publisher_PHPUnitTestResults_Task($this));
@@ -54,24 +55,22 @@ class Xinc_Plugin_Repos_Publisher_PHPUnitTestResults extends Xinc_Plugin_Base
      * Copies a file into a special test results directory for the build and
      * parse the xml file to generate statistics
      *
-     * @param Xinc_Build_Interface $build
-     * @param string $sourceFile
+     * @param Xinc_Build_Interface $build      The actuall build.
+     * @param string               $sourceFile The xml file to read out.
      *
-     * @return boolean
+     * @return boolean True if registering was ok, false if it failed.
      */
     public function registerResults(Xinc_Build_Interface $build, $sourceFile)
     {
-        $sourceFile = realpath($sourceFile);
-        
         $statusDir = Xinc::getInstance()->getStatusDir();
-        
+
         $projectDir = Xinc::getInstance()->getProjectDir();
-        
-        
+
         $subDir = $build->getStatusSubDir();
-        $fullDir = $statusDir . DIRECTORY_SEPARATOR . $subDir . DIRECTORY_SEPARATOR . self::TESTRESULTS_DIR;
+        $fullDir = $statusDir . DIRECTORY_SEPARATOR . $subDir
+            . DIRECTORY_SEPARATOR . self::TESTRESULTS_DIR;
         $targetFile = $fullDir . DIRECTORY_SEPARATOR . basename($sourceFile);
-        
+
         /**
          * Verify that the source is in the projectdir
          */
@@ -83,11 +82,16 @@ class Xinc_Plugin_Repos_Publisher_PHPUnitTestResults extends Xinc_Plugin_Base
              * 
              * Future: run Xinc in a chroot environment per project
              */
-            $build->error('Registering test results: ' . $sourceFile . '->' . $targetFile . ' failed.');
-            $build->error('-- ' . $sourceFile . ' is not within project dir. Security Problem.');
+            $build->error(
+                'Registering test results: ' . $sourceFile . '->' . $targetFile
+                . ' failed.'
+            );
+            $build->error(
+                '-- ' . $sourceFile . ' is not within project dir. Security Problem.'
+            );
             return false;
         }
-        
+
         if (!file_exists($fullDir)) {
             mkdir($fullDir, 0755, true);
         }
@@ -103,7 +107,7 @@ class Xinc_Plugin_Repos_Publisher_PHPUnitTestResults extends Xinc_Plugin_Base
             } else {
                 $status = 'FAILURE';
             }
-        } else {
+        } elseif (is_file($sourceFile)) {
             $res = copy($sourceFile, $targetFile);
             if ($res) {
                 chmod($targetFile, 0755);
@@ -112,50 +116,89 @@ class Xinc_Plugin_Repos_Publisher_PHPUnitTestResults extends Xinc_Plugin_Base
                  * register statistics
                  */
                 $build->getInternalProperties()->set('phpunit.file', $targetFile);
-                $this->_parseXml($build, $targetFile);
+                $this->generateStats($build, $targetFile);
             } else {
                 $status = 'FAILURE';
             }
+        } else {
+            $build->error('The phpunit file ' . $sourceFile . ' couldn\'t be read.');
+            return false;
         }
-        $build->info('Registering test results: ' . $sourceFile . '->' . $targetFile . ', result: ' . $status);
+        $build->info(
+            'Registering test results: ' . $sourceFile . '->' . $targetFile
+            . ', result: ' . $status
+        );
         return $res;
     }
     
-    private function _parseXml(Xinc_Build_Interface $build, $file)
+    /**
+     * Generates statistsics and saves them into the build.
+     *
+     * @param Xinc_Build_Interface $build      The actuall build.
+     * @param string               $sourceFile The xml file to read stats from.
+     *
+     * @return boolean True if registering was ok, false if it failed.
+     */
+    private function generateStats(Xinc_Build_Interface $build, $file)
     {
         try {
-
-            
-            $xml = new SimpleXMLElement(file_get_contents($file));
-            $testSuites = $xml->xpath("//testsuite");
-            
-            $testSuiteCount = 0;
-            $failureCount = 0;
-            $errorCount = 0;
-            $timeTaken = 0;
-            foreach ($testSuites as $name => $suite) {
-                $attributes = $suite->attributes();
-                $testSuiteCount += $attributes['tests'];
-                $failureCount += $attributes['failures'];
-                $errorCount += $attributes['errors'];
-                $timeTaken += (float)$attributes['time'];
-            }
-            
-            
-            
-            $unitStats = array('numberOfTests' => $testSuiteCount,
-                               'numberOfFailures' => $failureCount,
-                               'numberOfErrors' => $errorCount,
-                               'totalTime' => round($timeTaken, 8) );
             /**
-             * set the statistics data
+             * @var string Contains content of the XML file
              */
-            foreach ($unitStats as $statName => $value) {
-                $build->getStatistics()->set('phpunit.' . $statName, $value);
+            $strContent = file_get_contents($file);
+
+            if ('' !== $strContent) {
+                $arStats = $this->parseXml($strContent);
+
+                /**
+                 * set the statistics data
+                 */
+                foreach ($arStats as $strName => $mValue) {
+                    $build->getStatistics()->set('phpunit.' . $strName, $mValue);
+                }
             }
         } catch (Exception $e) {
-            //var_dump($e);
-            Xinc_Logger::getInstance()->error('Could not parse phpunit xml: ' . $e->getTraceAsString());
+            Xinc_Logger::getInstance()->error(
+                'Could not parse phpunit xml: ' . $e->getTraceAsString()
+            );
         }
+    }
+
+
+    /**
+     * Reads the XML file, generates statistsics and saves them into the build.
+     *
+     * @param string $strContent The XML to parse.
+     *
+     * @return boolean True if registering was ok, false if it failed.
+     */
+    private function parseXml($strContent)
+    {
+        $testSuiteCount = 0;
+        $failureCount = 0;
+        $errorCount = 0;
+        $timeTaken = 0;
+
+        $xml = new SimpleXMLElement();
+        $testSuites = $xml->xpath("//testsuite");
+
+        foreach ($testSuites as $name => $suite) {
+            $attributes = $suite->attributes();
+            $testSuiteCount += $attributes['tests'];
+            $failureCount += $attributes['failures'];
+            $errorCount += $attributes['errors'];
+            $timeTaken += (float)$attributes['time'];
+        }
+
+
+
+        $arStats = array(
+            'numberOfTests' => $testSuiteCount,
+            'numberOfFailures' => $failureCount,
+            'numberOfErrors' => $errorCount,
+            'totalTime' => round($timeTaken, 8)
+        );
+
+        return $arStats;
     }
 }
