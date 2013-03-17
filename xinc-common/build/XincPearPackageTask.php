b64b46a68028d3c6ad75d691774d611408d9deb5
Alexander Opitz <opi99@users.noreply.github.com>
2013-03-17 10:39:00 +0000

    Issue 238: New build.xml structure for packaging.

--- xinc-common/build/XincPearPackageTask.php
+++
@@ -1,6 +0,0 @@
-        $package->addReplacement('xinc.ini.tpl', 'package-info', '@VERSION@', 'version');
-        $package->addReplacement('scripts/xinc-uninstall', 'pear-config', '@PHP_BIN@', 'php_bin');
-        $package->addReplacement('scripts/xinc-uninstall', 'pear-config', '@BIN_DIR@', 'bin_dir');
-        $package->addReplacement('etc/init.d/xinc', 'pear-config', '@BIN_DIR@', 'bin_dir');
-        $package->addReplacement('bin/xinc', 'pear-config', '@PHP_BIN@', 'php_bin');
-        $package->addReplacement('Xinc/scripts/pear-install.sh', 'pear-config', '@data-dir@','data_dir');
