5c85afcda68ad581b330481af01b3e1e37936272
Alexander Opitz <opi99@users.noreply.github.com>
2013-07-29 09:59:43 +0000

    #232: Clean Up CodingStyle and .htaccess

--- xinc-web/web/handler.php
+++ xinc-web/web/handler.php
@@ -1,6 +1,6 @@
 <?php
 
-global $_NO_RUN,$_NO_RUN_REASON;;
+global $_NO_RUN,$_NO_RUN_REASON;
 
 require_once 'Xinc/Ini.php';
 require_once 'Xinc/Gui/Handler.php';
@@ -8,21 +8,22 @@ require_once 'Xinc/Gui/Handler.php';
 if (isset($_NO_RUN) && $_NO_RUN === true) {
     switch ($_NO_RUN_REASON) {
         case 1:
-            echo "<h4>Xinc/Ini.php could not be loaded. Please make sure the PEAR directory is in the include path</h4>";
+            echo 'Xinc/Ini.php could not be loaded. Please make sure the PEAR directory is in the include path';
             break;
         case 2:
-            echo "<h4>you need to enable the mod_rewrite module</h4>";
+            echo 'you need to enable the mod_rewrite module';
             break;
         default:
-            echo "<h4>Configuration problems prevent Xinc-GUI from working properly</h4>";
+            echo 'Configuration problems prevent Xinc-GUI from working properly';
     }
 } else {
     /*
      * 
      * get environment variables or read config.xml
      */
-    $handler=new Xinc_Gui_Handler(Xinc_Ini::getInstance()->get('etc','xinc') . DIRECTORY_SEPARATOR . "system.xml",Xinc_Ini::getInstance()->get('status_dir','xinc'));
-    
+    $handler = new Xinc_Gui_Handler(
+        Xinc_Ini::getInstance()->get('etc', 'xinc') . DIRECTORY_SEPARATOR . 'system.xml',
+        Xinc_Ini::getInstance()->get('status_dir', 'xinc')
+    );
     $handler->view();
-
 }
