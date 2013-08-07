204674dddacb6f1ce4d81f241af851b5d41f6f10
Alexander Opitz <opi99@users.noreply.github.com>
2013-08-07 16:41:40 +0000

    Issue 232: Plugin CleanUp and Coding Style

--- xinc-web/src/Xinc/Gui/Widget/Abstract.php
+++ xinc-web/src/Xinc/Gui/Widget/Abstract.php
@@ -7,8 +7,8 @@
  *
  * @category  Development
  * @package   Xinc.Gui.Widget
- * @author    Arno Schneider <username@example.org>
- * @copyright 2007 Arno Schneider, Barcelona
+ * @author    Alexander Opitz <opitz.alexander@gmail.com>
+ * @copyright 2013 Alexander Opitz, Leipzig
  * @license   http://www.gnu.org/copyleft/lgpl.html GNU/LGPL, see license.php
  *            This file is part of Xinc.
  *            Xinc is free software; you can redistribute it and/or modify
@@ -27,10 +27,32 @@
  * @link      http://code.google.com/p/xinc/
  */
 
-require_once 'Xinc/Plugin/Interface.php';
+require_once 'Xinc/Gui/Widget/Interface.php';
 
-interface Xinc_Gui_Widget_Interface
+abstract class Xinc_Gui_Widget_Abstract implements Xinc_Gui_Widget_Interface
 {
+    /**
+     * The bundled plugin
+     *
+     * @var Xinc_Plugin_Interface
+     */
+    protected $plugin;
+
+    /**
+     * True if this Widget handles exceptions self.
+     *
+     * @var boolean
+     */
+    protected $hasExceptionHandler = false;
+
+
+    /**
+     * List of Sub Extensions
+     *
+     * @var array
+     */
+    protected $extensions = array();
+
     /**
      * Constructor for a Widget
      * 
@@ -41,8 +63,10 @@ interface Xinc_Gui_Widget_Interface
      *
      * @param Xinc_Plugin_Interface $plugin
      */
-    public function __construct(Xinc_Plugin_Interface $plugin);
-
+    public function __construct(Xinc_Plugin_Interface $plugin)
+    {
+        $this->plugin = $plugin;
+    }
     /**
      * The Xinc_Gui_Handler fires different events on the
      * Widgets (see Xinc_Gui_Event)
@@ -51,13 +75,16 @@ interface Xinc_Gui_Widget_Interface
      * A normal page load-event is Xinc_Gui_Event::PAGE_LOAD
      *
      * @param integer $eventId
+     *
+     * @return void
      */
-    public function handleEvent($eventId);
+    public function handleEvent($eventId)
+    {
+    }
 
     /**
-     * returns the Pathnames for which the Widget 
-     * wants to register itself for execution
-     * 
+     * returns the Pathnames for which the Widget wants to register itself for execution
+     *
      * getPaths() returns array('/dashboard', '/olddashboard');
      * Widget is called for: 
      *   http://mydomain.com/dashboard
@@ -66,48 +93,82 @@ interface Xinc_Gui_Widget_Interface
      *     BUT NOT FOR
      *   http://mydomain.com/dashboard/
      *
+     * @return array
      */
-    public function getPaths();
+    public function getPaths()
+    {
+        return array();
+    }
 
     /**
      * Is called after all widgets have
      * been registered. This is the place where widgets need 
      * to register the hooks for another Widget
      *
+     * @return void
      */
-    public function init();
+    public function init()
+    {
+    }
 
     /**
      * get the defined hooks of this widget
      * 
-     * Hooks can be used to allow other widgets to
-     * extend this widget
+     * Hooks can be used to allow other widgets to extend this widget
+     *
      * @return array
      */
-    public function getExtensionPoints();
+    public function getExtensionPoints()
+    {
+        return array();
+    }
 
     /**
      * Register an extensions
      *
      * @param string $extensionPoint
      * @param Xinc_Gui_Widget_Extension $extension extension
+     *
+     * @return void
      */
-    public function registerExtension($extensionPoint, $extension);
+    public function registerExtension($extensionPoint, $extension)
+    {
+        if (!isset($this->extensions[$extensionPoint])) {
+            $this->extensions[$extensionPoint] = array();
+        }
+        $this->extensions[$extensionPoint][] = $extension;
+    }
 
     /**
      * Returns the registered extensions
      *
      * @return array
      */
-    public function getExtensions();
+    public function getExtensions()
+    {
+        return $this->extensions;
+    }
 
     /**
+     * Returns if this widget handles exceptions themself.
+     *
      * @return boolean
      */
-    public function hasExceptionHandler();
+    public function hasExceptionHandler()
+    {
+        return $this->hasExceptionHandler;
+    }
 
     /**
-     * @param Exception $e
+     * Handles an exception while view.
+     *
+     * @param Exception $e The Exception to handle
+     *
+     * @return void
      */
-    public function handleException(Exception $e);
+    public function handleException(Exception $e) {
+        if ($this->hasExceptionHandler) {
+            throw new Exception('Widget has no exception handler but needs to handle an exception');
+        }
+    }
 }
