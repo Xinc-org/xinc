72b4536b769347a90ad6d6da6f091f10833dfb18
Alexander Opitz <opi99@users.noreply.github.com>
2014-06-03 10:57:03 +0000

    Issue 254: Add base exception class.

---
+++ Xinc.Core/src/Exception.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Xinc - Continuous Integration.
+ * This is the base exception.
+ *
+ * PHP version 5
+ *
+ * @category  Development
+ * @package   Xinc.Core
+ * @author    Alexander Opitz <opitz.alexander@gmail.com>
+ * @copyright 2007 David Ellis, One Degree Square
+ * @license   http://www.gnu.org/copyleft/lgpl.html GNU/LGPL, see license.php
+ *            This file is part of Xinc.
+ *            Xinc is free software; you can redistribute it and/or modify
+ *            it under the terms of the GNU Lesser General Public License as
+ *            published by the Free Software Foundation; either version 2.1 of
+ *            the License, or (at your option) any later version.
+ *
+ *            Xinc is distributed in the hope that it will be useful,
+ *            but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *            GNU Lesser General Public License for more details.
+ *
+ *            You should have received a copy of the GNU Lesser General Public
+ *            License along with Xinc, write to the Free Software Foundation,
+ *            Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @link       http://code.google.com/p/xinc/
+ */
+
+namespace Xinc\Core;
+
+class Exception extends \Exception
+{
+}
